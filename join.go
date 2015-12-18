581026a30de49e76abe866ca4895371a85012814
alecandra <alexandra.iulus@kalon.ro>
2015-12-18 15:34:54 +0200

    i join feature

--- join.go
+++ join.go
@@ -1,10 +1,36 @@
 package main
 
 import (
-    "fmt"
     "net/http"
+    "log"
+    "encoding/json"
+    "redis"
 )
 
-func join(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "joining")
+type KeyVal struct {
+    Name string
+}
+
+func join(w http.ResponseWriter, req *http.Request) {
+	// create the Redis client
+	spec := redis.DefaultSpec().Db(13).Password("")
+	client, e := redis.NewSynchClientWithSpec(spec)
+	if e != nil {
+		log.Println("failed to create the client", e)
+		return
+	}
+		
+	decoder := json.NewDecoder(req.Body)
+	reqKeyVal := KeyVal{}
+	err := decoder.Decode(&reqKeyVal)
+	
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("reqKeyVal = " + reqKeyVal.Name)
+	
+	client.Set(reqKeyVal.Name, []byte("false"))
+	log.Println("Value set in Redis")
+	 
 }
