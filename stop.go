fa93d4c8cb0721c0e41edd767c8c4dc97c675b28
ionutradu23 <ionut.radu@kalon.ro>
2015-12-21 11:45:42 +0200

    Fix stop endpoint

--- stop.go
+++ stop.go
@@ -5,8 +5,17 @@ import (
     "net/http"
     "redis"
     "log"
-
+    // "encoding/json"
+    // "bytes"
 )
+
+
+type slackMsg struct {
+    Text     string `json:"text"`
+    Username string `json:"username"` // Anonymous animal sender
+    Channel  string `json:"channel"`  // Recipient
+    Token    string `json:"token"`
+}
 
 func stop(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Secret Santa assignments:")
@@ -24,11 +33,28 @@ func stop(w http.ResponseWriter, r *http.Request) {
     }
     client.Set(keys[0], []byte(keys[len(keys) - 1]))
 
+
     for i := 0; i < len(keys); i++ {
     	val, _ := client.Get(keys[i])
-    	fmt.Fprint(w,string(keys[i]))
+    	
+  //   	form := url.Values{}
+  //   	form.Add("channel", string(keys[i]))
+  //   	form.Add("text", string(val[:]))
+  //   	form.Add("token", "xoxb-17007351888-ytHkTD0IrKyAkjM45Mu3U0Sq")
+  //   	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		// payload, _ := json.Marshal(slackMsg{
+	 //        Text:     string(val[:]),
+	 //        Channel:  string(keys[i]),
+	 //        Username: "secretsanta",
+	 //        Token: "xoxb-17007351888-ytHkTD0IrKyAkjM45Mu3U0Sq",
+	 //    })
+
+  //   	http.Post("https://slack.com/api/chat.postMessage", "application.json", bytes.NewBuffer(payload))
+
+    	fmt.Fprint(w, string(keys[i]))
     	fmt.Fprint(w," - ")
-    	fmt.Fprintln(w,string(val[:]))
+    	fmt.Fprintln(w, string(val[:]))
     }
 
     client.Flushall()
